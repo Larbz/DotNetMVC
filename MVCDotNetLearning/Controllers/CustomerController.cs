02636ffcac315e95e452b71a40392d8ea0828ef3
Luis Arroyo <luisarroyo0330@gmail.com>
2023-10-11 11:57:39 -0500

    Start

---
+++ MVCDotNetLearning/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDotNetLearning.Models;
+
+namespace MVCDotNetLearning.Controllers;
+
+public class CustomerController : Controller
+{
+    public static List<Customer> customers = new List<Customer>()
+    {
+        new Customer() { Id = 101, Name = "King", Amount = 12000 },
+        new Customer() { Id = 102, Name = "King", Amount = 12000 },
+    };
+    // GET
+    public IActionResult Index()
+    {
+        ViewBag.Message = "Customer Management System";
+        ViewBag.CustomerCount = customers.Count;
+        ViewBag.CustomerList = customers;
+        return View();
+    }
+
+    public IActionResult Details()
+    {
+        return View();
+    }
+    
+    public IActionResult Message()
+    {
+        return View();
+    }
+}
---
+++ MVCDotNetLearning/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCDotNetLearning.Models;
+
+public class Customer
+{
+
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Amount { get; set; }
+}
